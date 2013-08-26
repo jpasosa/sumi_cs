11c4a73f62a2f7aad66c0728d3a9a09719e2e452
jpasosa <jpasosa@gmail.com>
2013-08-26 16:24:04 -0300

    all_models :: empiezo productos. Hay repo_productos, get_productos y action_productos

---
+++ application/modules_core/all_models/models/repo_productos.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Repo_productos extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->config('estados');
+	}
+
+
+
+
+
+
+}
+
+?>
