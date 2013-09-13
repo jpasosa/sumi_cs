50075422db3b1ecbb72fa9a9d20c96a0c41909b0
jpasosa <jpasosa@gmail.com>
2013-09-13 18:07:58 -0300

    categorias :: no mostraba la lista de productos en el menu izquierdo SOLVED

--- application/modules_core/productos/views/menu_izq.php
+++ application/modules_core/productos/views/menu_izq.php
@@ -5,14 +5,15 @@
 </a>
 
 <?php if ($section == 'productos.listar' || $section == 'productos.ver' || $section == 'productos.editar' || $section == 'productos.configuracion'
-			|| $section == 'productos.confListarCategorias'): ?>
+			|| $section == 'productos.confListarCategorias' || $section == 'productos.add_categoria' || $section == 'productos.editar_categoria'): ?>
 	<a href="<?php echo site_url('productos/add');?>">
 		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/add.png'; ?>" alt="listar" title="Agregar Productos" width="40" height="40" />
 	</a>
 <?php endif ?>
 
 <?php if ($section == 'productos.add' || $section == 'productos.ver' || $section == 'productos.editar' || $section == 'productos.configuracion'
-			|| $section == 'entrada_stock.nueva_entrada' || $section == 'productos.confListarCategorias'): ?>
+			|| $section == 'entrada_stock.nueva_entrada' || $section == 'productos.confListarCategorias' || $section == 'productos.add_categoria'
+			|| $section == 'productos.editar_categoria'): ?>
 	<a href="<?php echo site_url('productos/listar');?>">
 		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/list.png'; ?>" alt="listar" title="Listar Productos" width="40" height="40" />
 	</a>
