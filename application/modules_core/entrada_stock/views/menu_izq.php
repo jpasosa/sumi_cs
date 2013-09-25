9befc9a9075247bc0462135028ac6c5e900c3ab3
jpasosa <jpasosa@gmail.com>
2013-09-25 19:56:59 -0300

    Entrada Stock :: arreglos de maquetación del grocery_crud

--- application/modules_core/entrada_stock/views/menu_izq.php
+++ application/modules_core/entrada_stock/views/menu_izq.php
@@ -6,13 +6,13 @@
 
 <?php if ( isset($show_add) && $show_add): ?>
 	<a href="<?php echo site_url('entrada_stock/nueva_entrada/add');?>">
-		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/add.png'; ?>" alt="listar" title="Agregar Productos" width="40" height="40" />
+		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/add.png'; ?>" alt="listar" title="Agregar Entradas al Stock" width="40" height="40" />
 	</a>
 <?php endif; ?>
 
 <?php if ( isset($show_list) && $show_list ): ?>
 	<a href="<?php echo site_url('entrada_stock/nueva_entrada');?>">
-		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/list.png'; ?>" alt="listar" title="Listar Productos" width="40" height="40" />
+		<img class="list" src="<?php echo ASSETS . 'frontend/images/productos/list.png'; ?>" alt="listar" title="Listar las Entradas al Stock" width="40" height="40" />
 	</a>
 <?php endif; ?>
 
