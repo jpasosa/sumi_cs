1bc6e4fce3d5ba9f207154cd353d2c0cb1bdbb4b
jpasosa <jpasosa@gmail.com>
2013-12-09 17:52:27 -0300

    Reestringo para que solamente se pueda utilizar ABM de productos y ABM de categorías

--- application/modules_core/homepage/views/homepage.php
+++ application/modules_core/homepage/views/homepage.php
@@ -9,20 +9,20 @@
 	</div>
 	<div class="box">
 		<a href="<?php echo site_url('stock_actual/listar');?>">
-			<img src="<?php echo ASSETS . 'frontend/images/stock_actual.png'; ?>" width="64" height="64" alt="stock" />
+			<img src="#" width="64" height="64" alt="stock" />
 			<span class="h3">Stock Actual</span>
 			<span class="p">Visualizar el stock actual.</span>
 		</a>
 	</div>
 	<div class="box">
-		<a href="<?php echo site_url('entrada_stock/nueva_entrada');?>">
+		<a href="#">
 			<img src="<?php echo ASSETS . 'frontend/images/ingreso_stock.png'; ?>" width="64" height="52" alt="ingreso" />
 			<span class="h3">Entrada de Stock</span>
 			<span class="p">Ingresar productos al Stock.</span>
 		</a>
 	</div>
 	<div class="box">
-		<a href="<?php echo site_url('remitos/agregar');?>">
+		<a href="#">
 			<img src="<?php echo ASSETS . 'frontend/images/salida_stock.png'; ?>" width="64" height="55" alt="egreso" />
 			<span class="h3">Remitos</span>
 			<span class="p">Realizar un Remito.</span>
