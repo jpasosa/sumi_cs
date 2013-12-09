1cae12a90ccd812171b26a4a5f7862cd141d3345
jpasosa <jpasosa@gmail.com>
2013-12-09 16:52:57 -0300

    defino generalidades. Mensajes de validación sin recargar página, en castellano en HTML5

--- web/assets/frontend/js/general.js
+++ web/assets/frontend/js/general.js
@@ -13,4 +13,17 @@ $(document).ready(function() {
 		$(".alert-success").alert('close');
 	}, 7000);
 
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("Campo Obligatorio, por favor completarlo.");
+            }
+        };
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+        };
+    }
+
 })
